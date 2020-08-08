1221a60874bdeafc507de1f40a1cd4732af8a282
sexyuck <sexyuck@gmail.com>
2020-08-08 17:39:18 +0900

    Initial commit

---
+++ chat-batch/src/main/java/com/leeryu/chatbatch/ChatBatchApplication.java
@@ -0,0 +1,13 @@
+package com.leeryu.chatbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatBatchApplication.class, args);
+	}
+
+}
---
+++ chat-batch/src/test/java/com/leeryu/chatbatch/ChatBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeryu.chatbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
